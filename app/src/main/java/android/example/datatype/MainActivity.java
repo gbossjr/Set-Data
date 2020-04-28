1b033a2c9166794ef32d3a0006ec65dd600c33db
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-28 04:59:37 -0800

    first commit

---
+++ app/src/main/java/android/example/datatype/MainActivity.java
@@ -0,0 +1,29 @@
+package android.example.datatype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
+
+    }
+}
